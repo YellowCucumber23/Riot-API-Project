a43491795a32c8045a490ad340b8abbb64c53841
YellowCucumber23 <84460615+YellowCucumber23@users.noreply.github.com>
2022-07-05 18:15:45 -0400

    display data

--- graphs/src/App.js
+++ graphs/src/App.js
@@ -20,11 +20,19 @@ function App() {
     <div>
       <div class="container">
         <div class="center">
-          <button onClick= {() => window.location.reload(false)} class = "noot">
-            Refresh Page!</button>
+          <button onClick= {() => window.location.reload(false)} className = "noot">
+            Refresh Data!</button>
         </div>
       </div>
       <RolesDoughnutChart></RolesDoughnutChart>
+
+      {(typeof data.win === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+          data.win.map((win, i) => (
+            <p key={i}>{win}</p>
+          ))
+      )}
     </div>
 
   );
